f246d9bd82c98138186e3480684bf8ad4fa59821
classify_glb <classify_glb@qq.com>
2020-03-19 19:43:27 +0800

    有向图的节点类

---
+++ src/main/java/com/zy/gragh/GraphNode.java
@@ -0,0 +1,34 @@
+package com.zy.gragh;
+
+import com.zy.entity.Ontology;
+import com.zy.entity.Service;
+
+import java.util.List;
+
+public class GraphNode {
+    /**
+     * input和output节点可以封装成一个空的服务，有用的只有本体和type
+     */
+    private Service service;
+
+    private List<GraphNode> nexts;
+
+    public GraphNode() {
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    public List<GraphNode> getNexts() {
+        return nexts;
+    }
+
+    public void setNexts(List<GraphNode> nexts) {
+        this.nexts = nexts;
+    }
+}
