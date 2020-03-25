3b5416e23703158c561a8696f39ac45b2528a93d
classify_glb <classify_glb@qq.com>
2020-03-25 14:59:22 +0800

    增加了Comparaple接口

--- src/main/java/com/zy/gragh/GraphNode.java
+++ src/main/java/com/zy/gragh/GraphNode.java
@@ -4,7 +4,7 @@ import com.zy.entity.Service;
 
 import java.util.List;
 
-public class GraphNode {
+public class GraphNode implements Comparable<GraphNode> {
     /**
      * input和output节点可以封装成一个空的服务，有用的只有本体和type
      */
@@ -29,5 +29,18 @@ public class GraphNode {
 
     public void setNexts(List<GraphNode> nexts) {
         this.nexts = nexts;
+    }
+
+    @Override
+    public int compareTo(GraphNode o) {
+        // 根据Qos对节点进行排序
+        double qosTotal = this.getService().getQosTotal();
+        double qosTotal_ = o.getService().getQosTotal();
+        if (qosTotal + 1e5 < qosTotal_)       // 为了保证浮点数的精度，使用1e5
+            return -1;
+        else if (qosTotal > qosTotal_ + 1e5)
+            return 1;
+        else
+            return 0;
     }
 }
